4d2d1239a95cf63fb1c96a496acd5dc7faaa817a
Aafthab Ashraff <aafthab.ashraff@gmail.com>
2022-06-19 12:29:21 +0100

    Gatsby file structure created

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <>
+    <h1>Aafthab Ashraff</h1>
+    <h2>Full-Stack Software Developer</h2>
+    <p>My Projects</p>
+    </>
+  )
+}
