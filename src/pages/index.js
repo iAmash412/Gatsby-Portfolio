5f0c6074e4798be4e1d6dc8b234c628400d5da6e
Aafthab Ashraff <aafthab.ashraff@gmail.com>
2022-06-19 13:16:38 +0100

    Components folder and Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div>
-    <h1>Aafthab Ashraff</h1>
-    <h2>Full-Stack Software Developer</h2>
-    <h3>Based in London, United Kingdom</h3>
-    <p>My Projects</p>
-    </div>
+    <section>
+      <Navbar />
+      <div>
+        <h1>Aafthab Ashraff</h1>
+        <h2>Full-Stack Software Developer</h2>
+        <h3>Based in London, United Kingdom</h3>
+        <p>My Projects</p>
+      </div>
+    </section>
   )
 }
